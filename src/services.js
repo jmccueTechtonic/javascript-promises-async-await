dd9defe9f5ee3c51f8d06a1b21b0d24a4bab3585
Joshua McCue <joshua.mccue@techtonic.com>
2020-11-12 17:36:49 -0500

    Completed module

--- src/services.js
+++ src/services.js
@@ -5,14 +5,24 @@ export function fetchWithTimeout(delay) {
 
 export function fetchMovies() {
   return fetch("./data/movies.json")
-  .then(response => response.json())
-  .then(movies => movies)
-  .catch(error => console.log(error));
+    .then(response => response.json())
+    .then(movies => movies)
+    .catch(error => console.log(error));
 }
 
 export function fetchBooks() {
   return fetch("./data/books.json")
-  .then(response => response.json())
-  .then(books => books)
-  .catch(error => console.log(error));
+    .then(response => response.json())
+    .then(books => books)
+    .catch(error => console.log(error));
+}
+
+export async function asyncFetchMovies() {
+  try {
+    const response = await fetch("/data/movies.json");
+    const results = await response.json();
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
 }
